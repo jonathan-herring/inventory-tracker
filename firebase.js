52b6abdb1360e4a9a44d74bebb8f4db0754bd2aa
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-08-02 15:13:44 -0700

    first commit

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAV__ueNQi4fO7Zks9KWz_NWn_1hFTtf98",
+  authDomain: "inventory-tracker-dbe6f.firebaseapp.com",
+  projectId: "inventory-tracker-dbe6f",
+  storageBucket: "inventory-tracker-dbe6f.appspot.com",
+  messagingSenderId: "387081289528",
+  appId: "1:387081289528:web:457e5c76b65260ffc20bcd",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
