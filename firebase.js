7cf4fe2a01cdc71b475de7c03f844210489e2289
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-08-04 16:54:56 -0700

    firebase

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -10,8 +11,11 @@ const firebaseConfig = {
   projectId: "inventory-tracker-dbe6f",
   storageBucket: "inventory-tracker-dbe6f.appspot.com",
   messagingSenderId: "387081289528",
-  appId: "1:387081289528:web:457e5c76b65260ffc20bcd",
+  appId: "1:387081289528:web:bf59a7265c5bdc49c20bcd",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const firestore = getFirestore(app);
+
+export { firestore };
